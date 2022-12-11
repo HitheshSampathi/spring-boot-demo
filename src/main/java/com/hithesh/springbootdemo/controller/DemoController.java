9bc2a0cb918fba61633b0312caf14d8317835f1b
Hithesh Sampathi <hithesh.sampathi@gmail.com>
2022-12-11 20:49:47 +0530

    Spring Security and Swagger development
    Added the Spring security Configuration and also swagger-ui

--- src/main/java/com/hithesh/springbootdemo/controller/DemoController.java
+++ src/main/java/com/hithesh/springbootdemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.hithesh.springbootdemo.controller;
 
 import com.hithesh.springbootdemo.Repository.DemoRepository;
 import com.hithesh.springbootdemo.dto.Demo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ public class DemoController {
     private DemoRepository repository;
 
     @GetMapping("/getDemoResponse")
+    @ApiOperation(value="This Endpoint is used to get the static response")
     public String getDemoResponse(){
         return " This is an demo response ";
     }
 
     @GetMapping("/getDemoDbResponse")
+    @ApiOperation(value="This Endpoint is used to get the  data stored in database")
     public List<Demo> getDemoDbResponse(){
         return repository.getDemoDetails();
     }
